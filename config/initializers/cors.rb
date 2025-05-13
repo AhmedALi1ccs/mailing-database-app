c08f09399f8aaf9179b3c5c988f33691c8dc6189
Ahmedali270402 <ahmed@goccs.net>
2025-05-13 20:46:06 +0300

    Fix CORS configuration to allow requests from GitHub Pages

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,13 +1,12 @@
 # config/initializers/cors.rb
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    # Add your GitHub Pages domain to the allowed origins
     origins ['http://localhost:3000', 'https://ahmedali1ccs.github.io']
     
     resource '*',
       headers: :any,
       methods: [:get, :post, :put, :patch, :delete, :options, :head],
-      expose: ['Content-Type', 'Content-Disposition'], # Important for file downloads
+      expose: ['Content-Type', 'Content-Disposition'],
       credentials: false
   end
 end
