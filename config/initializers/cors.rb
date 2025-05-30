3b54733986c8e682b86d8e1426aad726e69a0044
Ahmedali270402 <ahmed@goccs.net>
2025-05-30 21:07:54 +0300

    update

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,4 +1,3 @@
-# config/initializers/cors.rb
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     origins ['http://localhost:3000', 'https://ahmedali1ccs.github.io']
@@ -6,7 +5,6 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
     resource '*',
       headers: :any,
       methods: [:get, :post, :put, :patch, :delete, :options, :head],
-      expose: ['Content-Type', 'Content-Disposition'],
       credentials: false
   end
 end
