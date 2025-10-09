570346ef08e3b950efc2aee70277ebf2f31cc288
Ahmedali270402 <ahmed@goccs.net>
2025-10-09 20:37:58 +0300

    render-deploy

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -1,5 +1,14 @@
 #!/usr/bin/env bash
 set -o errexit
 
+# Update bundler first
+gem update --system 3.3.22
+
+# Remove Gemfile.lock and regenerate
+rm -f Gemfile.lock
+
+# Install dependencies
 bundle install
+
+# Run database migrations
 bundle exec rails db:migrate
